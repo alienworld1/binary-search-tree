7cfeda4e1a45b367276d8c98a04908e0d9f223f4
alienworld1 <astronomyguy0030@gmail.com>
2024-03-01 20:56:56 +0530

    Build a BST from an array of numbers

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,5 +1,20 @@
-function Node(value, left=null, right=null) {
-    return {value, left, right};
+function Node(data, left=null, right=null) {
+    return {data, left, right};
+}
+
+const buildTree = (array, low = 0, high) => {
+    if (low > high) {
+        return null;
+    }
+
+    high = high ?? array.length - 1;
+    const mid = Math.trunc((low + high) / 2);
+    const root = Node(
+        array[mid],
+        buildTree(array, low, mid - 1),
+        buildTree(array, mid + 1, high)
+    );
+    return root;
 }
 
 export default class BinarySearchTree {
@@ -7,6 +22,28 @@ export default class BinarySearchTree {
     #root;
 
     constructor(array) {
-        this.#root = buildTree(array);
+        const sorted = array.sort((a, b) => a - b);
+        this.#root = buildTree(sorted);
     }
+
+    get root() {
+        return this.#root;
+    }
+
+
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+export {prettyPrint};
